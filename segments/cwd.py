acefe3c90c9e0ad193ed04a2888c2a92ffdc8edd
Pierre Louis Aublin <plaublin@alphanumericvision.fr>
2015-05-11 11:17:14 +0200

    print the path with / so that we can copy/paste it easily

--- segments/cwd.py
+++ segments/cwd.py
@@ -21,17 +21,27 @@ def add_cwd_segment():
     if len(names) > max_depth:
         names = names[:2] + [u'\u2026'] + names[2 - max_depth:]
 
+    if names[0] == '~':
+        if Color.HOME_SPECIAL_DISPLAY:
+            powerline.append('', Color.HOME_FG, Color.HOME_BG)
+        else:
+            powerline.append('', Color.PATH_FG, Color.PATH_BG)
+    else:
+            powerline.append(' ', Color.PATH_FG, Color.PATH_BG,
+                    powerline.separator_thin, Color.SEPARATOR_FG)
+
     if not powerline.args.cwd_only:
         for n in names[:-1]:
             if n == '~' and Color.HOME_SPECIAL_DISPLAY:
-                powerline.append(' %s ' % n, Color.HOME_FG, Color.HOME_BG)
+                powerline.append('%s' % n, Color.HOME_FG, Color.HOME_BG)
             else:
-                powerline.append(' %s ' % n, Color.PATH_FG, Color.PATH_BG,
+                powerline.append('%s' % n, Color.PATH_FG, Color.PATH_BG,
                     powerline.separator_thin, Color.SEPARATOR_FG)
 
+    s = ('%s'%names[-1]) + powerline.separator_thin + ' '
     if names[-1] == '~' and Color.HOME_SPECIAL_DISPLAY:
-        powerline.append(' %s ' % names[-1], Color.HOME_FG, Color.HOME_BG)
+        powerline.append(s, Color.HOME_FG, Color.HOME_BG)
     else:
-        powerline.append(' %s ' % names[-1], Color.CWD_FG, Color.PATH_BG)
+        powerline.append(s, Color.CWD_FG, Color.PATH_BG)
 
 add_cwd_segment()
